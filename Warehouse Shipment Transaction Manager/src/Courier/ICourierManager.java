f8f6856a923a9b591af1b455d64f2e0bc39ea647
Steven Deligero <228299180+phikuldagreat@users.noreply.github.com>
2025-09-26 23:08:47 +0800

    Update ICourierManager.java

--- Warehouse Shipment Transaction Manager/src/Courier/ICourierManager.java
+++ Warehouse Shipment Transaction Manager/src/Courier/ICourierManager.java
@@ -3,11 +3,8 @@ package Courier;
 import models.Truck;
 import java.util.List;
 
-/**
- * Interface for managing courier operations (trucks).
- */
 public interface ICourierManager {
     void acceptTruck(Truck truck);
-    void declineTruck(String plate);
     List<Truck> getAllTrucks();
+	void declineTruck(String code, String plate);
 }
